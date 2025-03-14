6edf9016030328c4b6a459c3638d9a8fdbd2c02a
Bharat Bhangale <cs.bharat.dev@gmail.com>
2025-03-14 16:33:11 +0530

    add Clerk authentication integration; update Navbar for user state and modify main entry point

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,13 @@
-import React, { useContext } from "react";
+import React from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
     const isCourseListPage = location.pathname.includes("/course-list");
+
+    const { openSignIn } = useClerk();
+    const { user } = useUser();
 
     return (
         <div
@@ -17,24 +21,43 @@ const Navbar = () => {
             />
             <div className="hidden md:flex items-center gap-5 text-gray-500">
                 <div className="flex items-center gap-5">
-                    <button>Become Educator</button>|{" "}
-                    <Link to="/my-enrollments">My Enrollments</Link>
+                    {user && (
+                        <>
+                            <button>Become Educator</button> |{" "}
+                            <Link to="/my-enrollments">My Enrollments</Link>
+                        </>
+                    )}
                 </div>
 
-                <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
-                    Create Account
-                </button>
+                {user ? (
+                    <UserButton />
+                ) : (
+                    <button
+                        className="bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer"
+                        onClick={() => openSignIn()}
+                    >
+                        Create Account
+                    </button>
+                )}
             </div>
 
             {/* For Phone Screens */}
             <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
-                <div className="flex items-center gap-5">
-                    <button>Become Educator</button>|{" "}
-                    <Link to="/my-enrollments">My Enrollments</Link>
+                <div className="flex items-center gap-1 sm:gap-2 max:sm:text-xs cursor-pointer">
+                    {user && (
+                        <>
+                            <button>Become Educator</button>|{" "}
+                            <Link to="/my-enrollments">My Enrollments</Link>
+                        </>
+                    )}
                 </div>
-                <button>
-                    <img src={assets.user_icon} alt="" />
-                </button>
+                {user ? (
+                    <UserButton />
+                ) : (
+                    <button onClick={() => openSignIn()} className="cursor-pointer">
+                        <img src={assets.user_icon} alt="" />
+                    </button>
+                )}
             </div>
         </div>
     );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,20 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store/store.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ClerkProvider>
   </BrowserRouter>
 );
