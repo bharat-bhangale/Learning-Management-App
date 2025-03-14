a64923341792846f5090399c5714020ae889644f
Bharat Bhangale <cs.bharat.dev@gmail.com>
2025-03-14 22:18:12 +0530

    add Hero component to Home page; update styling and layout for improved user experience; bump Tailwind CSS version

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Hero from "../../components/student/Hero";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col items-center text-center space-y-7">
+      <Hero />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
