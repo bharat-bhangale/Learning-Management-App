461454d3320e8bc854ad16a248b70d6f9b1aa231
Bharat Bhangale <cs.bharat.dev@gmail.com>
2025-03-14 15:08:33 +0530

    refactor routing by removing centralized routes and defining routes directly in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { routes } from "./routes/AppRouter";
+import Home from "./pages/student/Home";
+import CourseList from "./pages/student/CoursesList";
+import CourseDetails from "./pages/student/CourseDetails";
+import MyEnrollments from "./pages/student/MyEnrollments";
+import Player from "./pages/student/Player";
+import Loading from "./components/student/Loading";
+import Educator from "./pages/educator/Educator";
+import Dashboard from "./pages/educator/Dashboard";
+import AddCourse from "./pages/educator/AddCourse";
+import MyCourses from "./pages/educator/MyCourses";
+import StudentEnrolled from "./pages/educator/StudentEnrolled";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        {routes.map(({ path, element, name }) => (
-          <Route key={name} path={path} element={element} />
-        ))}
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CourseList />} />
+        <Route path="/course-list/:input" element={<CourseList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentEnrolled />} />
+        </Route>
       </Routes>
     </div>
   );
