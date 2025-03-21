888d050a119ef518c7035b73388818d4c298b3fc
Bharat Bhangale <cs.bharat.dev@gmail.com>
2025-03-21 22:16:07 +0530

    add Navbar integration; implement user greeting and profile image display in educator layout

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/educator/Navbar";
 
 const Educator = () => {
   return (
     <>
-      <div>Educator</div>
-      <div>
-        <Outlet />
+      <div className="text-default min-h-screen bg-white">
+        <Navbar />
+        <div>
+          <Outlet />
+        </div>
       </div>
     </>
   );
